8492360ceec3c0c0da5c32177df1c1632f20caae
Kris Wood <kriswd0@gmail.com>
2021-05-17 10:11:35 +1200

    Add responsive table cols

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
 
 // Gold chart
 
-function loadChart() {
-    var options = {
-        bullion: 'gold',
-        currency: 'GBP',
-        timeframe: '5y',
-        containerDefinedSize: false,
-        switchCurrency: true,
-        displayLatestPriceLine: true,
-        switchTimeframe: true
-    };
+// function loadChart() {
+//     var options = {
+//         bullion: 'gold',
+//         currency: 'GBP',
+//         timeframe: '5y',
+//         containerDefinedSize: false,
+//         switchCurrency: true,
+//         displayLatestPriceLine: true,
+//         switchTimeframe: true
+//     };
     
-    var chartBV = new BullionVaultChart(options, 'chartContainer');
+//     var chartBV = new BullionVaultChart(options, 'chartContainer');
     
-    // return document.getElementById("chartContainer").innerHTML = chartBV;
+//     // return document.getElementById("chartContainer").innerHTML = chartBV;
 
-} 
+// } 
+
+// var kidNames = ['lily', 'freddie', 'ruby']
+
+// function capitaliseName(name) {
+//     var x = name[0].toUpperCase() + name.substring(1)
+// }
+
+// kidNames.forEach((kid) => {
+//     var kid = {
+//         name: capitaliseName(kid),
+//         purchases: 'Test'
+//     }
+// }); 
+
+
+var lily = {
+        name: 'Lily',
+        purchased: ["23/3/19"],
+    }
+
+var fred = {
+        name: 'Freddie',
+        purchased: ["1/1/20", "1/2/20"]
+    }
+
+var kids = [lily, fred]
+
+console.log(kids)
 
 
